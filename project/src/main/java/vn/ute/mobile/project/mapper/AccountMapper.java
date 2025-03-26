b577858441c0d577a30134e27654abb35eb8c3a1
Hao079231 <haotrinh2003.com@gmail.com>
2025-03-26 15:10:58 +0700

    fix code

--- project/src/main/java/vn/ute/mobile/project/mapper/AccountMapper.java
+++ project/src/main/java/vn/ute/mobile/project/mapper/AccountMapper.java
@@ -1,15 +1,12 @@
 package vn.ute.mobile.project.mapper;
 
-import java.util.List;
 import org.mapstruct.BeanMapping;
-import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 import vn.ute.mobile.project.dto.account.AccountDto;
-import vn.ute.mobile.project.form.account.CreateAccountForm;
 import vn.ute.mobile.project.model.Account;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE,
@@ -18,19 +15,7 @@ public interface AccountMapper {
   @Mapping(source = "username", target = "username")
   @Mapping(source = "fullname", target = "fullname")
   @Mapping(source = "email", target = "email")
-  @Mapping(source = "password", target = "password")
-  @BeanMapping(ignoreByDefault = true)
-  @Named("fromCreateAccountToEntity")
-  Account fromCreateAccountToEntity(CreateAccountForm createAccountForm);
-
-  @Mapping(source = "username", target = "username")
-  @Mapping(source = "fullname", target = "fullname")
-  @Mapping(source = "email", target = "email")
-  @Mapping(source = "password", target = "password")
   @BeanMapping(ignoreByDefault = true)
   @Named("fromAccountToDto")
   AccountDto fromAccountToDto(Account account);
-
-  @IterableMapping(elementTargetType = AccountDto.class, qualifiedByName = "fromAccountToDto")
-  List<AccountDto> fromAccountToDtoList(List<Account> accounts);
 }
